6ba024da389eb153db7518b556f874a8440cf87f
Karin Jonsson <jonsson.karin.ae@gmail.com>
2017-01-30 20:49:44 +0100

    some blog tests

---
+++ Hemtenta_Karin_Jonsson/HemtentaTester/BlogTest.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using NUnit.Framework;
+using HemtentaTdd2017.blog;
+using HemtentaTdd2017;
+using Moq;
+
+namespace HemtentaTester
+{
+    [TestFixture]
+    public class BlogTest
+    {
+        Blog blog;
+
+        [SetUp]
+        public void Setup()
+        {
+            blog = new Blog();
+        }
+
+        [Test]
+        public void LoginUser_ReturnsTrueIfLoginSucceeds()
+        {
+            User u = new User("testUser");
+
+            var mockAuth = new Mock<IAuthenticator>();
+            mockAuth.Setup(x => x.GetUserFromDatabase(u.Name)).Returns(u);
+            blog.SetAuthenticator(mockAuth.Object);
+
+            blog.LoginUser(u);
+
+            Assert.That(blog.UserIsLoggedIn, Is.True);
+            mockAuth.Verify(x => x.GetUserFromDatabase(u.Name), Times.Once);
+
+        }
+
+        [Test]
+        public void LoginUser_ReturnsFalseIfLoginFails()
+        {
+            User dbUser = new User("testUser");
+            User u = new User(dbUser.Name);
+            u.Password = "hejsansvejsan";
+
+            var mockAuth = new Mock<IAuthenticator>();
+            mockAuth.Setup(x => x.GetUserFromDatabase(u.Name)).Returns(dbUser);
+            blog.SetAuthenticator(mockAuth.Object);
+
+            blog.LoginUser(u);
+
+            Assert.That(blog.UserIsLoggedIn, Is.False);
+            mockAuth.Verify(x => x.GetUserFromDatabase(u.Name), Times.Once);
+        }
+
+        [Test]
+        public void LoginUser_NullUserThrowsException()
+        {
+            Assert.That(() => blog.LoginUser(null), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void LoginUser_NoUserInDbThrowsException()
+        {
+            User uNull = null;
+            User user = new User("username");
+
+            var mockAuth = new Mock<IAuthenticator>();
+            mockAuth.Setup(x => x.GetUserFromDatabase(user.Name)).Returns(uNull);
+            blog.SetAuthenticator(mockAuth.Object);
+
+            Assert.That(() => blog.LoginUser(user), Throws.TypeOf<NullReferenceException>());
+            mockAuth.Verify(x => x.GetUserFromDatabase(user.Name), Times.Once);
+
+        }
+
+        [Test]
+        public void LogoutUser_NullUserThrowsException()
+        {
+            Assert.That(() => blog.LogoutUser(null), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void LogoutUser_LogsOutUserWhoIsLoggedIn()
+        {
+            User u = new User("LogMeOutUser");
+
+            var mockAuth = new Mock<IAuthenticator>();
+            mockAuth.Setup(x => x.GetUserFromDatabase(u.Name)).Returns(u);
+            blog.SetAuthenticator(mockAuth.Object);
+
+            blog.LoginUser(u);
+            blog.LogoutUser(u);
+
+            bool result = blog.UserIsLoggedIn;
+
+            Assert.That(result, Is.False);
+
+        }
+
+
+        // För att publicera en sida måste Page vara
+        // ett giltigt Page-objekt och användaren
+        // måste vara inloggad.
+        // Returnerar true om det gick att publicera,
+        // false om publicering misslyckades och
+        // exception om Page har ett ogiltigt värde.
+        //bool PublishPage(Page p);
+
+        [Test]
+        public void PublishPage_InvalidPageThrowsException()
+        {
+            Assert.That(() => blog.PublishPage(null), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        //[Test]
+        //public void PublishPage_UserNotLoggedInThrowsException()
+        //{
+
+        //}
+
+        [Test]
+        public void PublishPage_SuccessRetursTrue()
+        {
+            User u = new User("Publisher");
+            blog.LoginUser(u);
+            
+            Page p = new Page();
+            bool result = blog.PublishPage(p);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void PublishPage_FailReturnsFalse()
+        {
+            //if user is not logged in? Can it fail any other way?
+            Page p = new Page();
+            bool result = blog.PublishPage(p);
+            Assert.That(result, Is.True);
+            
+        }
+
+        [TestCase(null, "Hello", "Message")]
+        [TestCase("", "Hello", "Message")]
+        [TestCase("NotAddress", "Hello", "Message")] //bestämde att det måste likna en mejladress
+
+        [TestCase("hej@svejsan.se", null, "Message")]
+        [TestCase("hej@svejsan.se", "", "Message")] //bestämde att en tom sträng inte är giltig
+
+        [TestCase("hej@svejsan.se", "Hello", null)]
+        [TestCase("hej@svejsan.se", "Hello", "")] //bestämde att en tom sträng inte är giltig
+        public void SendEmail_InvalidParameterThrowsException(string address, string caption, string body)
+        {
+            Assert.That(() => blog.SendEmail(address, caption, body), Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void SendEmail_ValidAndLoggedInReturnsTrue()
+        {
+            User u = new User("Hannibal");
+            blog.LoginUser(u);
+
+            int result = blog.SendEmail("test@mail.net", "header", "body");
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SendEmail_ValidNotLoggedInFalse()
+        {
+            int result = blog.SendEmail("test@mail.net", "header", "body");
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        // För att skicka e-post måste användaren vara
+        // inloggad och alla parametrar ha giltiga värden.
+        // Returnerar 1 om det gick att skicka mailet,
+        // 0 annars.
+
+
+    }
+}
---
+++ Hemtenta_Karin_Jonsson/Hemtenta_Karin_Jonsson/blog/Blog.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HemtentaTdd2017.blog
+{
+    public class Blog : IBlog
+    {
+        IAuthenticator auth;
+
+        public void SetAuthenticator(IAuthenticator auth)
+        {
+            this.auth = auth;
+        }
+
+        public bool UserIsLoggedIn { get; private set; }
+
+
+        // Försöker logga in en användare. Man kan
+        // se om inloggningen lyckades på property
+        // UserIsLoggedIn.
+        // Kastar ett exception om User är null.
+        public void LoginUser(User u)
+        {
+            if (u == null)
+                throw new ArgumentNullException();
+
+            User dbUser = auth.GetUserFromDatabase(u.Name);
+
+            if (dbUser == null)
+                throw new NullReferenceException();
+
+            if (u.Password == dbUser.Password)
+            {
+                UserIsLoggedIn = true;
+            }
+            
+        }
+
+        // Försöker logga ut en användare. Kastar
+        // exception om User är null.
+        public void LogoutUser(User u)
+        {
+            if (u == null)
+                throw new ArgumentNullException();
+
+            if (UserIsLoggedIn)
+                UserIsLoggedIn = false;
+
+        }
+
+
+        // För att publicera en sida måste Page vara
+        // ett giltigt Page-objekt och användaren
+        // måste vara inloggad.
+        // Returnerar true om det gick att publicera,
+        // false om publicering misslyckades och
+        // exception om Page har ett ogiltigt värde.
+        public bool PublishPage(Page p)
+        {
+            throw new NotImplementedException();
+        }
+
+        // För att skicka e-post måste användaren vara
+        // inloggad och alla parametrar ha giltiga värden.
+        // Returnerar 1 om det gick att skicka mailet,
+        // 0 annars.
+        public int SendEmail(string address, string caption, string body)
+        {
+            const string emailRegex = @"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*"
+                                    + "@"
+                                    + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$";
+
+            if (string.IsNullOrEmpty(caption))
+                throw new ArgumentException("Null or empty strings not valid", caption);
+
+            if (string.IsNullOrEmpty(body))
+                throw new ArgumentException("Null or empty strings not valid", body);
+
+            if (string.IsNullOrEmpty(address) || !Regex.IsMatch(address, emailRegex))
+                throw new ArgumentException("Not a valid e-mail address", address);
+
+
+            return UserIsLoggedIn ? 1 : 0;
+
+        }
+    }
+}
